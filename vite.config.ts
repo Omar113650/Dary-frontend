73e35250cd2ef2382595989353bef1d3858fee41
Omar113650 <omarelhelaly520@gmail.com>
2026-09-21 04:28:04 +0300

    update integrate to frontend

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/v1': {
-        target: 'http://localhost:8003',
+        target: 'https://dary-student-housing.vercel.app',
         changeOrigin: true,
       },
     },
